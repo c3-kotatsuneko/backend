511f4a220b250e036c13e662fff835ab6e56ac18
Tanakaryuki <tnk3257.rt@gmail.com>
2024-09-07 02:47:54 +0900

    setting: 🔧 TODO!

--- internal/app/constants/constants.go
+++ internal/app/constants/constants.go
@@ -1,8 +1,9 @@
 package constants
 
 const (
-	IntervalTicker    int    = 1
-	CountDownTimer    int    = 3
+	IntervalTicker int = 1
+	CountDownTimer int = 3
+	//TODO: 本番では300にする
 	TimeOutTimer      int    = 30
 	RoomStatusWaiting string = "waiting"
 	RoomStatusPlaying string = "playing"
