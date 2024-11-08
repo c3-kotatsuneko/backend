761db553c689f923a712323bb9b7c75adb2e8a80
K-Kizuku <kotani.kizuku@gmail.com>
2024-11-09 04:08:57 +0900

    fix errCh close bug

--- internal/app/presentation/switcher/event_switcher.go
+++ internal/app/presentation/switcher/event_switcher.go
@@ -36,8 +36,8 @@ func (s *EventSwitcher) Switch(ctx context.Context, doneCh chan struct{}, conn *
 	// 終了したい場合は、doneCh<-struct{}{}を呼ぶ
 	// doneCh := make(chan struct{})
 	// defer close(doneCh)
+	defer close(errCh)
 	go func() {
-		defer close(errCh)
 		for {
 			select {
 			case err := <-errCh:
@@ -51,90 +51,87 @@ func (s *EventSwitcher) Switch(ctx context.Context, doneCh chan struct{}, conn *
 			}
 		}
 	}()
+
+L:
 	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			errCh <- err
-			break
-
-		}
-		fmt.Println("messageType: ", messageType)
-		switch messageType {
-		case websocket.TextMessage:
-			var msg any
-			if err := json.Unmarshal(p, &msg); err != nil {
+		select {
+		case <-doneCh:
+			break L
+		default:
+			messageType, p, err := conn.ReadMessage()
+			if err != nil {
 				errCh <- err
-				break
 
 			}
-		case websocket.BinaryMessage:
-			var msg rpc.GameStatusRequest
-			if err := proto.Unmarshal(p, &msg); err != nil {
-				fmt.Println("err: ", err)
-				errCh <- err
-				break
-
-			}
-			switch msg.Mode {
-			case resources.Mode_MODE_TIME_ATTACK:
-			case resources.Mode_MODE_MULTI:
-				switch msg.Event {
-				case resources.Event_EVENT_ENTER_ROOM:
-					if err := s.eventServise.EnterRoom(ctx, msg.RoomId, msg.Player, conn); err != nil {
-						errCh <- err
-						break
-
-					}
-				case resources.Event_EVENT_GAME_START:
-					if err := s.eventServise.GameStart(ctx, msg.RoomId); err != nil {
-						errCh <- err
-						break
-
-					}
-					go func() {
-						// s.eventServise.CountDown(ctx, errCh, doneCh, msg.RoomId)
-						s.eventServise.Timer(ctx, errCh, doneCh, msg.RoomId)
-					}()
-				case resources.Event_EVENT_STATS:
-					if err := s.eventServise.Stats(ctx, msg.RoomId, msg.Player); err != nil {
-						errCh <- err
-						break
-
-					}
-				case resources.Event_EVENT_STACK_BLOCK:
-					if err := s.eventServise.StackBlock(ctx, msg.RoomId, msg.Player); err != nil {
-						errCh <- err
-						break
-
-					}
-				case resources.Event_EVENT_RESULT:
-					if err := s.eventServise.Result(ctx, msg.RoomId); err != nil {
-						errCh <- err
-						break
-
-					}
-				default:
-					fmt.Println("unhandling event")
-					errCh <- errors.New("unhandling event")
-					break
+			fmt.Println("messageType: ", messageType)
+			switch messageType {
+			case websocket.TextMessage:
+				var msg any
+				if err := json.Unmarshal(p, &msg); err != nil {
+					errCh <- err
 
 				}
-			case resources.Mode_MODE_TRAINING:
-			case resources.Mode_MODE_UNKNOWN:
-				errCh <- errors.New("unknown mode")
-				break
+			case websocket.BinaryMessage:
+				var msg rpc.GameStatusRequest
+				if err := proto.Unmarshal(p, &msg); err != nil {
+					fmt.Println("err: ", err)
+					errCh <- err
 
+				}
+				switch msg.Mode {
+				case resources.Mode_MODE_TIME_ATTACK:
+				case resources.Mode_MODE_MULTI:
+					switch msg.Event {
+					case resources.Event_EVENT_ENTER_ROOM:
+						if err := s.eventServise.EnterRoom(ctx, msg.RoomId, msg.Player, conn); err != nil {
+							errCh <- err
+
+						}
+					case resources.Event_EVENT_GAME_START:
+						if err := s.eventServise.GameStart(ctx, msg.RoomId); err != nil {
+							errCh <- err
+
+						}
+						go func() {
+							// s.eventServise.CountDown(ctx, errCh, doneCh, msg.RoomId)
+							s.eventServise.Timer(ctx, errCh, doneCh, msg.RoomId)
+						}()
+					case resources.Event_EVENT_STATS:
+						if err := s.eventServise.Stats(ctx, msg.RoomId, msg.Player); err != nil {
+							errCh <- err
+
+						}
+					case resources.Event_EVENT_STACK_BLOCK:
+						if err := s.eventServise.StackBlock(ctx, msg.RoomId, msg.Player); err != nil {
+							errCh <- err
+
+						}
+					case resources.Event_EVENT_RESULT:
+						if err := s.eventServise.Result(ctx, msg.RoomId); err != nil {
+							errCh <- err
+
+						}
+					default:
+						fmt.Println("unhandling event")
+						errCh <- errors.New("unhandling event")
+
+					}
+				case resources.Mode_MODE_TRAINING:
+				case resources.Mode_MODE_UNKNOWN:
+					errCh <- errors.New("unknown mode")
+
+				default:
+					errCh <- errors.New("unhandling mode")
+
+				}
 			default:
-				errCh <- errors.New("unhandling mode")
-				break
+				fmt.Println("unhandling message type")
+				errCh <- nil
 
 			}
-		default:
-			fmt.Println("unhandling message type")
-			errCh <- nil
-			break
 		}
+
 	}
-	doneCh <- struct{}{}
 	return nil
+
 }
